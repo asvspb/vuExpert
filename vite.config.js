52f5d7945e613d2eb0e0e85320595ab7bcbb8116
asv-spb <asvdevpro@gmail.com>
2025-11-30 10:46:01 +0300

    chore(config): add SCSS preprocessor options to suppress legacy JS API deprecation warnings

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ import { configDefaults } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api']
+      }
+    }
+  },
   test: {
     environment: 'jsdom',
     exclude: [...configDefaults.exclude, 'e2e/*'],
