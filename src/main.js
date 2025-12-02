323548bda41e980533e898b8991d873c6b5b101a
asv-spb <asvdevpro@gmail.com>
2025-12-02 08:31:33 +0300

    feat(deps): add pinia and vue-router dependencies with routing and state management setup
    - Add pinia for state management and vue-router for routing
    - Create router configuration with home route
    - Set up pinia store initialization
    - Update main.js to use pinia and router plugins
    - Fix rgba syntax error in mixins.scss shadow definition

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import router from './router'
+import pinia from './stores'
+import './styles/main.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
